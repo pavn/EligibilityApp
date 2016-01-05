61c9276face0f66c6d8e6dcf8cfa406f1733fc04
Pavan Narayn <pavan.narayanan@gmail.com>
2016-01-05 13:15:58 -0500

    Create adc_calculations.js
    initial commit with functions revised

---
+++ adc_calculations.js
@@ -0,0 +1,40 @@
+
+
+                            /****************************************************************************************
+                            *                       ADC FINANCIAL ELIGIBILITY CALCULATIONS                          *
+                            *                              Pavan Narayanan, 2015                                    *
+                            *****************************************************************************************/
+
+/*
+Input Variables: 
+B.Income Information
+Number in Family (1-30) also default and else -- id is familyheadcount
+Income frequency 1 and 2: w b m and monthly income -- incFreq1, incFreq2, incAmt1, incAmt2
+Source of Income 1 and 2: dropdown box selected -- source1, source2
+monthly unearned income
+Shelter type dropdown box selected - id is localDistrict, ShelterID
+fuel type dropdown box selected -- fuelID
+
+*/
+
+function bNeedEnergySupp() {
+    var headcount = document.getElementById("familyheadcount").value;
+    switch (headcount) {
+            
+    }
+    
+
+
+
+
+}
+
+
+
+function shelterAllowance() {}
+function fuel() {}
+function grossIncome1() {}
+function grossIncome2() {}
+function grossEarnedIncome() {}
+function workExp1() {}
+function workExp2() {}
